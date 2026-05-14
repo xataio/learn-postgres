706eee9c3a20c7e19d9c5011e7af39e3ecc6f7e4
Carlos Pérez-Aradros Herce <exekias@gmail.com>
2026-05-14 16:36:49 +0200

    minor fixes

--- components/PoweredByXata.tsx
+++ components/PoweredByXata.tsx
@@ -15,7 +15,7 @@ export function PoweredByXata() {
         <svg
           viewBox="0 0 134 135"
           aria-label="Xata"
-          className="h-3.5 w-3.5"
+          className="h-4 w-4"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
